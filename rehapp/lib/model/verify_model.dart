a53570e908a18da92568712386a1e2c0ae04a0fc
nnuba3 <nnuba3@gatech.edu>
2023-02-27 11:13:23 -0500

    Commenting out verify_model.dart file since not used

--- rehapp/lib/model/verify_model.dart
+++ rehapp/lib/model/verify_model.dart
@@ -1,21 +1,22 @@
-class VerifyResponseModel {
-  final String error;
-
-  VerifyResponseModel({this.error = ""});
-
-  factory VerifyResponseModel.fromJson(Map<String, dynamic> json) {
-    print('you better be rurnign');
-    return VerifyResponseModel(
-      error: json["error"] ?? "",
-    );
-  }
-
-  factory VerifyResponseModel.fromEmpty() {
-    return VerifyResponseModel(
-      error: "",
-    );
-  }
-}
+//commenting out this file it is not used
+// class VerifyResponseModel {
+//   final String error;
+//
+//   VerifyResponseModel({this.error = ""});
+//
+//   factory VerifyResponseModel.fromJson(Map<String, dynamic> json) {
+//     print('you better be rurnign');
+//     return VerifyResponseModel(
+//       error: json["error"] ?? "",
+//     );
+//   }
+//
+//   factory VerifyResponseModel.fromEmpty() {
+//     return VerifyResponseModel(
+//       error: "",
+//     );
+//   }
+// }
 
 // class VerifyRequestModel {
 //   String emailToken;
